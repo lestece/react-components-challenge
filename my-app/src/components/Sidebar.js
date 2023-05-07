7ac112e9e71d5daffa206a74919e94995bf4b914
lestece <>
2023-05-07 17:08:24 +0000

    Use css modules to style Sidebar component

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
+import css from "./css/Sidebar.module.css"
 
-const Sidebar = () => <div> <a href="">My Photos</a> <a href="">My Illustrations</a> <a href="">My Paintings</a></div>;
+const Sidebar = () => <div className={css.sidebar}> <a href="">My Photos</a> <a href="">My Illustrations</a> <a href="">My Paintings</a></div>;
 
 export default Sidebar;
